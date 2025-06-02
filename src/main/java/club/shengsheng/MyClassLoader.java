a618f478e936c7925ca18b1d15ddc65613a308cd
IceProgrammer <13706531210@163.com>
2025-06-02 13:08:58 +0800

    完成自定义类加载器加载加密 class

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,9 +1,32 @@
 package club.shengsheng;
 
 
+import java.io.File;
+import java.nio.file.Files;
+
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        if (name.startsWith("tech.insight.ShengSheng")) {
+            String classPath = System.getProperty("user.dir").concat("/加密.class");
+            File file = new File(classPath);
+            try {
+                byte[] bytes = Files.readAllBytes(file.toPath());
+                // 减少一个 1
+                for (int i = 0; i < bytes.length; i++) {
+                    bytes[i] = (byte) (bytes[i] - 1);
+                }
+                // 返回生成的类
+                return defineClass(name, bytes, 0, bytes.length);
+            } catch (Exception e) {
+                throw new ClassNotFoundException(name);
+            }
+        }
+        // 其他类遵循双亲委派机制
+        return super.findClass(name);
+    }
 }
